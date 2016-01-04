da36277985c5f0fe90d7e163572bc6fbe0013818
Christian Kakesa <christian.kakesa@gmail.com>
2016-01-04 04:12:00 +0100

    Fix string symbol for old Ruby 2.

--- lib/netsoul/location.rb
+++ lib/netsoul/location.rb
@@ -12,21 +12,21 @@ module Netsoul
         'ext'.freeze
       end
 
-      # rubocop:disable Metrics/AbcSize
+      # rubocop:disable all
       def locations
         {
-          'lab-cisco-mid-sr': '10.251.'.freeze,
+          :'lab-cisco-mid-sr' => '10.251.'.freeze,
           etna: '10.245.'.freeze,
           lse: '10.227.42.'.freeze,
-          'sda-1': '10.227.4.'.freeze,
+          :'sda-1' => '10.227.4.'.freeze,
           lab: '10.227.'.freeze,
-          'lab-tcom': '10.226.7.'.freeze,
-          'lab-acu': '10.226.6.'.freeze,
-          'lab-console': '10.226.5.'.freeze,
-          'lab-mspe': '10.226.'.freeze,
+          :'lab-tcom' => '10.226.7.'.freeze,
+          :'lab-acu' => '10.226.6.'.freeze,
+          :'lab-console' => '10.226.5.'.freeze,
+          :'lab-mspe' => '10.226.'.freeze,
           epitanim: '10.225.19.'.freeze,
           epidemic: '10.225.18.'.freeze,
-          'sda-2': '10.225.10.'.freeze,
+          :'sda-2' => '10.225.10.'.freeze,
           cycom: '10.225.8.'.freeze,
           epx: '10.225.7.'.freeze,
           prologin: '10.225.6.'.freeze,
@@ -36,7 +36,7 @@ module Netsoul
           www: '10.223.106.'.freeze,
           episport: '10.223.104.'.freeze,
           epicom: '10.223.103.'.freeze,
-          'bde-epita': '10.223.100.'.freeze,
+          :'bde-epita' => '10.223.100.'.freeze,
           omatis: '10.223.42.'.freeze,
           ipsa: '10.223.15.'.freeze,
           lrde: '10.223.13.'.freeze,

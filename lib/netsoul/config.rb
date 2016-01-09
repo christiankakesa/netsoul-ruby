1f287d8b6aa403716c21b358ba72a2b9a6fe4053
Christian Kakesa <christian.kakesa@gmail.com>
2016-01-09 17:02:51 +0100

    Add environment variable capability for config setting. Bump to version 1.7.0.

--- lib/netsoul/config.rb
+++ lib/netsoul/config.rb
@@ -17,20 +17,33 @@ module Netsoul
 
     attr_reader :client_name
 
-    # rubocop:disable Metrics/AbcSize
+    # Supported environment variables:
+    #
+    # +NETSOUL_SERVER_HOST+: Netsoul server host, default 'ns-server.epita.fr'
+    # +NETSOUL_SERVER_PORT+: Netsoul server port, default 4242
+    # +NETSOUL_LOGIN+: IONIS account name
+    # +NETSOUL_SOCKS_PASSWORD+: IONIS socks password
+    # +NETSOUL_UNIX_PASSWORD+: IONIS unix password
+    # +NETSOUL_AUTH_METHOD+: Authentication method, default :std. Valid options are => @see +Config::AUTH_METHODS+
+    # +NETSOUL_STATE+: User status, default is :none. Valid options are => @see +Config::USER_STATES+
+    # +NETSOUL_LOCATION+: User location is free text of your position. If you ar in IONIS network an automatic mapping is proceed to detect your location
+    # +NETSOUL_USER_GROUP+: Free text specifying your promo or whatever else
+    # +NETSOUL_CLIENT_NAME+: Redefine the client name exposed to the Netsoul server
+    #
+    # rubocop:disable all
     def initialize(opts = {})
-      @server_host = opts.fetch(:server_host, 'ns-server.epita.fr'.freeze)
-      @server_port = Integer(opts.fetch(:server_port, 4242))
-      @login = opts.fetch(:login, 'ionis'.freeze)
-      @socks_password = opts.fetch(:socks_password, 'socks_password'.freeze)
-      @unix_password = opts.fetch(:unix_password, 'unix_password'.freeze)
-      @auth_method = AUTH_METHODS.include?(opts[:auth_method]) ? opts[:auth_method] : :std
-      @state = USER_STATES.include?(opts[:state]) ? opts[:state] : :none
-      @location = opts.fetch(:location, 'Home'.freeze)
-      @user_group = opts.fetch(:user_group, 'ETNA_2008'.freeze)
+      @server_host = ENV['NETSOUL_SERVER_HOST'] || opts.fetch(:server_host, 'ns-server.epita.fr'.freeze)
+      @server_port = Integer(ENV['NETSOUL_SERVER_PORT'] || opts.fetch(:server_port, 4242))
+      @login = ENV['NETSOUL_LOGIN'] || opts.fetch(:login, 'ionis'.freeze)
+      @socks_password = ENV['NETSOUL_SOCKS_PASSWORD'] || opts.fetch(:socks_password, 'socks_password'.freeze)
+      @unix_password = ENV['NETSOUL_UNIX_PASSWORD'] || opts.fetch(:unix_password, 'unix_password'.freeze)
+      @auth_method = (ENV['NETSOUL_AUTH_METHOD'] || (AUTH_METHODS.include?(opts[:auth_method]) ? opts[:auth_method] : :std)).to_sym
+      @state = (ENV['NETSOUL_STATE'] || (USER_STATES.include?(opts[:state]) ? opts[:state] : :none)).to_sym
+      @location = ENV['NETSOUL_LOCATION'] || opts.fetch(:location, 'Home'.freeze)
+      @user_group = ENV['NETSOUL_USER_GROUP'] || opts.fetch(:user_group, 'ETNA_2008'.freeze)
       @user_connection_info = {}
 
-      @client_name = opts.fetch(:client_name, '(Netsoul-Ruby) -> { Christian Kakesa, since 2009}'.freeze)
+      @client_name = ENV['NETSOUL_CLIENT_NAME'] || opts.fetch(:client_name, '(Netsoul-Ruby) -> { Christian Kakesa, since 2009}'.freeze)
     end
 
     def build_user_connection_info(opts = {})

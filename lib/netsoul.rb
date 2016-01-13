1f08784e40f76643453f4376ac66b78b62889a56
Christian Kakesa <christian.kakesa@gmail.com>
2016-01-14 00:35:55 +0100

    Code review: move Netsoul::Location class to Netsoul::Message file because it's only used by Message class.

--- lib/netsoul.rb
+++ lib/netsoul.rb
@@ -1,6 +1,5 @@
 require_relative 'netsoul/config'
 require_relative 'netsoul/errors'
-require_relative 'netsoul/location'
 require_relative 'netsoul/logging'
 require_relative 'netsoul/message'
 require_relative 'netsoul/version'
--- lib/netsoul/message.rb
+++ lib/netsoul/message.rb
@@ -2,8 +2,6 @@ require 'base64'
 require 'digest/md5'
 require 'uri'
 
-require_relative 'location'
-
 module Netsoul
   class Message
     class << self
@@ -116,6 +114,56 @@ module Netsoul
 
       def _unescape(str)
         URI.unescape str
+      end
+    end
+  end
+
+  class Location
+    class << self
+      def get(ip)
+        locations.each do |key, val|
+          return "#{key}".freeze if ip.match(/^#{val}/)
+        end
+        'ext'.freeze
+      end
+
+      # rubocop:disable all
+      def locations
+        @locations ||= {
+            :'lab-cisco-mid-sr' => '10.251.'.freeze,
+            etna: '10.245.'.freeze,
+            lse: '10.227.42.'.freeze,
+            :'sda-1' => '10.227.4.'.freeze,
+            lab: '10.227.'.freeze,
+            :'lab-tcom' => '10.226.7.'.freeze,
+            :'lab-acu' => '10.226.6.'.freeze,
+            :'lab-console' => '10.226.5.'.freeze,
+            :'lab-mspe' => '10.226.'.freeze,
+            epitanim: '10.225.19.'.freeze,
+            epidemic: '10.225.18.'.freeze,
+            :'sda-2' => '10.225.10.'.freeze,
+            cycom: '10.225.8.'.freeze,
+            epx: '10.225.7.'.freeze,
+            prologin: '10.225.6.'.freeze,
+            nomad: '10.225.2.'.freeze,
+            assos: '10.225.'.freeze,
+            sda: '10.224.14.'.freeze,
+            www: '10.223.106.'.freeze,
+            episport: '10.223.104.'.freeze,
+            epicom: '10.223.103.'.freeze,
+            :'bde-epita' => '10.223.100.'.freeze,
+            omatis: '10.223.42.'.freeze,
+            ipsa: '10.223.15.'.freeze,
+            lrde: '10.223.13.'.freeze,
+            cvi: '10.223.7.'.freeze,
+            epi: '10.223.1.'.freeze,
+            pasteur: '10.223.'.freeze,
+            bocal: '10.42.42.'.freeze,
+            sm: '10.42.'.freeze,
+            vpn: '10.10.'.freeze,
+            adm: '10.1.'.freeze,
+            epita: '10.'.freeze
+        }
       end
     end
   end

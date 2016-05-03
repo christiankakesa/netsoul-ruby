8c0ec248cea6552901fe11a92404b07361535199
Christian Kakesa <christian.kakesa@gmail.com>
2016-05-03 23:24:45 +0200

    Rubocop review.

--- spec/memory_test.rb
+++ spec/memory_test.rb
@@ -7,9 +7,11 @@ Netsoul::Client.new({}).connect
 
 MemoryProfiler.report(trace: [Netsoul::Client]) do
   100.times do
+    # rubocop:disable all
     begin
       Netsoul::Client.new({}).connect
     rescue
     end
+    # rubocop:enable all
   end
 end.pretty_print
